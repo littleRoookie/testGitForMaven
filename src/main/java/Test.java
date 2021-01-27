d81801ab25b1152cb6c2cf071e9afe6572c7a017
gzx_glb <gzx_glb@uestc.com>
2021-01-27 11:48:42 +0800

    这是hot_fix分支首次提交的东西

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * @author Zhengxing Guan
+ * @create 2021-01-27 11:47
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("This is hot_fix branch");
+    }
+}
